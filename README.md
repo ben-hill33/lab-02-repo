348aeac63e36d1039979efa62b789e773ae122bd
BEN <benjamin.hill28@gmail.com>
2020-06-05 17:37:15 -0700

    completed feature 2

--- README.md
+++ README.md
@@ -5,7 +5,24 @@ Number and name of feature: Feature #1: Display images
 Estimate of time needed to complete: 3 hours
 
 Start time: 3:30
+Finish time: 4:30
 
-Finish time: 
+Actual time needed to complete: One hour
+
+Number and name of feature: Feature #2: Filter images
+
+Estimate of time needed to complete: 4 hours
+
+Start time: 4:30
+Finish time: 5:30 (next day)
+
+Actual time needed to complete: Seven and a half
+
+Number and name of feature: Feature #3: Style the Application
+
+Estimate of time needed to complete: 5 hours
+
+Start time: 3:30
+Finish time: 4:30
 
 Actual time needed to complete: 
--- js/app.js
+++ js/app.js
@@ -1,52 +1,66 @@
 'use strict';
 
 let globalCache = [];
+let allKeywords = [];
 
 // Constructor function
 function HornImage(horns) {
-  this.keyword = horns.keyword;
-  this.description = horns.description;
   this.title = horns.title;
   this.image_url = horns.image_url;
+  this.description = horns.description;
+  this.keyword = horns.keyword;
+  this.horns = horns.horns;
+
   globalCache.push(this);
+  addKeyWord(horns.keyword);
 }
+
+
 HornImage.prototype.render = function () {
-  let $hornsClone = $("#photo-template").clone();
-  $("main").append($hornsClone);
-  $hornsClone.find("img").attr("src", this.image_url);
-  $hornsClone.find("p").text(this.description);
-  $hornsClone.find("h3").text(this.title);
-  // $hornsClone.removeID('photo-template');
-  // $hornsClone.('id', this.name);
+  let template = $('#photo-template').html();
+  let $hornsClone = $('<section></section>');
+
+  $('main').append($hornsClone);
+  $hornsClone.html(template);
+  $hornsClone.find('h2').text(this.title);
+  $hornsClone.find('img').attr('src', this.image_url);
+  $hornsClone.find('p').text(this.description);
+  $hornsClone.attr('data-keyword', this.keyword);
 }
-// Create function that loops through globalCache & target keywords
-  function dropDownRender(horns) {
-    this.keyword = horns.keyword;
-  } 
-  globalCache.forEach(keyword => {
-    let optionTag = `<option value="${keyword}">${keyword}</option>`;
-    $('select').append(optionTag);
-  });
-  dropDownRender.prototype.render = function () {
-    let $hornsClone = $("#photo-template").clone();
-    $("header").append(dropDownRender);
-    dropDownRender.find("option").text(this.keyword);
+
+function addKeyWord(keyword){
+  if(!allKeywords.includes(keyword)){
+    allKeywords.push(keyword);
   }
-// ==============================================================
-HornImage.readJson = () => {
-  const ajaxSettings = {
-    method: 'get',
-    dataType: 'json'
-  };
-  $.ajax('data/page-1.json', ajaxSettings).then(data => {
-    data.forEach(item => {
-      let horns = new HornImage(item);
-      //call new dropdownRender function here
-      dropDownRender();
-      //Call handleDropdown function- use .then's to chain to ajax call
-      console.log(horns);
-      horns.render();
+}
+
+function addDropDownOptions (){
+  let $dropdown = $('select');
+  allKeywords.forEach(keyword => {
+    let $newOption = $(`<option value = "${keyword}">${keyword}</option>`);
+    $dropdown.append($newOption);
+  })
+}
+  
+
+ $.ajax('data/page-1.json', {
+   method: 'get',
+   dataType: 'json',
+ })
+  .then(horn => {
+    horn.forEach(horns => {
+      new HornImage(horns);
+    });
+    globalCache.forEach(item => {
+      item.render();
+    });
+    addDropDownOptions();
+    $('select').on('change', function(){
+      $('section').hide();
+      $('section').each((index, element) => {
+        if(this.value === $(element).attr('data-keyword')){
+          $(element).show();
+        };
+      });
     });
   });
-};
-$(() => HornImage.readJson());
